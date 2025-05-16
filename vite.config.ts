9a424c1f50d7c9a65a876b25e755e1551c38d239
Pukkok <krystalgggg@gmail.com>
2025-05-16 23:16:37 +0900

    [Fix] 빌드설정 부분 전체 제거, 예제 화면 불러오기로 변경

--- vite.config.ts
+++ vite.config.ts
@@ -1,37 +1,17 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import dts from 'vite-plugin-dts';
-import path from 'path';
-import { fileURLToPath } from 'url';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import path from 'path'
+import { fileURLToPath } from 'url'
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
 
 export default defineConfig({
-  plugins: [
-    react(),
-    dts({
-      insertTypesEntry: true,
-      exclude: ['examples', 'src/**/*.d.ts'],
-      rollupTypes: true
-    })
-  ],
-  build: {
-    lib: {
-      entry: path.resolve(__dirname, 'src/index.ts'),
-      name: 'AutoMergeTable',
-      fileName: (format) => `index.${format}.js`,
-      formats: ['es', 'cjs'],
+  root: 'examples',
+  plugins: [react()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src'),
     },
-    rollupOptions: {
-      external: ['react', 'react-dom'],
-      output: {
-        globals: {
-          react: 'React',
-          'react-dom': 'ReactDOM',
-        }
-      }
-    },
-    cssCodeSplit: false
   }
 });
