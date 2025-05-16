7ee4c7cbfce4134f8d3c464e38b2d5f8ad1a38c0
Pukkok <krystalgggg@gmail.com>
2025-05-16 10:47:18 +0900

    [Feat] merge table children 구조로 변경

--- src/components/AutoMergeTable.tsx
+++ src/components/AutoMergeTable.tsx
@@ -1,21 +1,15 @@
 import React from 'react'
-import { parseRowsToMatrix } from '../utils/parseRowsToMatrix'
-import { TableHeader } from '@/components/TableHeader'
-import { TableBody } from '@/components/TableBody'
+import styles from '@/styles/table.module.css'
 
 type Props = {
-  headers: string[]
-  rows: string[][]
-  onCellClick?: (value: string) => void
-}
+  children: React.ReactNode
+  className?: string
+} & React.TableHTMLAttributes<HTMLTableElement>
 
-export const AutoMergeTable = ({ headers, rows, onCellClick }: Props) => {
-  const matrix = parseRowsToMatrix(rows)
-
+export const AutoMergeTable = ({ children, className, ...rest }: Props) => {
   return (
-    <table style={{ borderCollapse: 'collapse', width: '100%' }}>
-      <TableHeader headers={headers} />
-      <TableBody matrix={matrix} onCellClick={onCellClick} />
+    <table className={`${styles.table} ${className ?? ''}`} {...rest}>
+      {children}
     </table>
   )
 }
