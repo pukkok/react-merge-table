9b25143f1e6c583e8ebff84dfdb8c39723ae2c6a
Pukkok <krystalgggg@gmail.com>
2025-05-16 11:40:22 +0900

    [Fix] rows 배열 타입 추가

--- src/components/TableBody.tsx
+++ src/components/TableBody.tsx
@@ -1,17 +1,26 @@
 import React from 'react'
-import { Cell } from '@/types/Cell'
-import { TableRow } from '@/components/TableRow'
+import styles from '../styles/table.module.css'
+import { Cell } from '../types/Cell'
+import { TableRow } from './TableRow'
+import { parseRowsToMatrix } from '../utils/parseRowsToMatrix'
 
 type Props = {
-  matrix: Cell[][]
-  onCellClick?: (value: string) => void
-}
+  rows: (string | number | (string | number)[])[][]
+  columnRenderers?: Record<number, (cell: Cell) => React.ReactNode>
+} & React.HTMLAttributes<HTMLTableSectionElement>
 
-export const TableBody = ({ matrix, onCellClick }: Props) => {
+export const TableBody = ({ rows, columnRenderers, className, ...rest }: Props) => {
+  const matrix = parseRowsToMatrix(rows)
+
   return (
-    <tbody>
+    <tbody className={className} {...rest}>
       {matrix.map((row, rowIndex) => (
-        <TableRow key={rowIndex} row={row} onCellClick={onCellClick} />
+        <TableRow
+          key={rowIndex}
+          row={row}
+          rowIndex={rowIndex}
+          columnRenderers={columnRenderers}
+        />
       ))}
     </tbody>
   )
