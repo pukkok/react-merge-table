81e37b5a4dcfce607e43f5d7d9870d5362b6483a
Pukkok <krystalgggg@gmail.com>
2025-05-18 06:20:22 +0900

    [Feat] 입력 받는값 any로 변경, value 정규화 완료

--- src/components/TableBody.tsx
+++ src/components/TableBody.tsx
@@ -1,22 +1,34 @@
 import React from 'react'
+import { Row, CellValue } from '../types/Row'
 import { Cell } from '../types/Cell'
 import { TableRow } from './TableRow'
 import { parseRowsToMatrix } from '../utils/parseRowsToMatrix'
 
-type Props = {
-  rows: (string | number | (string | number)[])[][]
+type TableBodyProps = {
+  rows: any[] // INFO: 입력 받는 값은 유연하게 받는다.
   columnRenderers?: Record<number, (cell: Cell) => React.ReactNode>
   defaultStyle?: boolean
 } & React.HTMLAttributes<HTMLTableSectionElement>
 
-export const TableBody = ({ rows, columnRenderers, defaultStyle = true, ...rest }: Props) => {
-  const matrix = parseRowsToMatrix(rows)
+export const TableBody = ({
+  rows,
+  columnRenderers,
+  defaultStyle = true,
+  ...rest
+}: TableBodyProps) => {
+  // INFO: 입력 형식이 CellValue[]일 경우 자동으로 key를 생성하여 Row로 변환
+  const normalizedRows: Row[] = rows.map((row, i) =>
+    Array.isArray(row) ? { key: `row-${i}`, data: row } : row
+  )
+
+  // INFO: 병합 처리 및 value 정규화
+  const matrix = parseRowsToMatrix(normalizedRows)
 
   return (
     <tbody {...rest}>
       {matrix.map((row, rowIndex) => (
         <TableRow
-          key={rowIndex}
+          key={normalizedRows[rowIndex].key}
           row={row}
           rowIndex={rowIndex}
           defaultStyle={defaultStyle}
