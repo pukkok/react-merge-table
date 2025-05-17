9bb35e885e191e6c81ddf6dc7ef0f8ecf76909ed
Pukkok <krystalgggg@gmail.com>
2025-05-18 07:01:33 +0900

    [Feat] 다중 컨텐츠 체크 불리언값 추가

--- src/types/Cell.ts
+++ src/types/Cell.ts
@@ -4,9 +4,10 @@ export type KeyedValue = {
 }
 
 export type Cell = {
-  value: unknown
-  content: KeyedValue
-  contents: KeyedValue[]
+  value: unknown                // 원본 입력값
+  content: KeyedValue           // 대표 값 (단일 렌더링용)
+  contents: KeyedValue[]        // 전체 값 배열 (다중 렌더링용)
+  hasMultiple: boolean              // contents가 복수인지 여부
   rowspan: number
   colspan: number
   render: boolean
