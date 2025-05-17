142cb490aa4e0470aafcac3f476e6346aa7fffe0
Pukkok <krystalgggg@gmail.com>
2025-05-18 07:34:18 +0900

    [Feat] 영문, 한글 버전 App 경로만만 추가

--- examples/main.tsx
+++ examples/main.tsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import AppDemo from './AppDemo'
+import AppKo from './App.ko'
+import AppEn from './App.en'
 
 const root = document.getElementById('root')!
 ReactDOM.createRoot(root).render(<AppDemo />)
